90cef62d1711b7564a378443d07e72ecccb2431d
strongforce1 <201811941@fit.edu.ph>
2021-06-12 08:41:54 +0800

    Migrate VedDSA signature method to kotlin

---
+++ src/main/java/org/creativeturbogiants/crypto/ed25519/veddsa_sigs.kt
@@ -0,0 +1,26 @@
+package org.creativeturbogiants.crypto.ed25519
+
+import org.creativeturbogiants.crypto.java.Sha512
+import org.creativeturbogiants.crypto.ed25519.gen_x
+
+/**
+ * Added VRF support, so java code won't rely on native c++ jni.
+ * https://github.com/signalapp/curve25519-java/pull/37
+ */
+
+object veddsa_sigs {
+    fun VRFsign(sha512provider: Sha512?, result: ByteArray?, privateKey: ByteArray?, message: ByteArray?, message_len: Int, random: ByteArray?): Int {
+
+        val ret_val = gen_x.generalized_xveddsa_25519_sign(result, privateKey, message, message_len.toLong(), random,
+                sha512provider, null, 0)
+        return ret_val
+    }
+
+    fun VRFverify(sha512provider: Sha512?, vrf_out: ByteArray?, signature: ByteArray?,
+                  publicKey: ByteArray?, message: ByteArray?, messagelen: Long): Int {
+
+        val ret_val = gen_x.generalized_xveddsa_25519_verify(vrf_out, signature, publicKey,
+                message, messagelen, null, 0, sha512provider)
+        return ret_val
+    }
+}
